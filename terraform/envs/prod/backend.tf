8ccab85a3167c24e6ecff0727b34555d9cafd0f4
SahanaSV19 <sahanagowda502@gmail.com>
2025-09-27 11:56:34 +0530

    Cleanup: ignore terraform cache, state, prod and staging envs

--- terraform/envs/prod/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    backend "s3" {
-        bucket = "prod-sahana-tf-bucket"
-        key = "prod/terraform.tfstate"
-        region = "us-east-1"
-        dynamodb_table = "terraform-lock"
-        encrypt = true
-    }
-}
--- terraform/envs/staging/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    backend "s3" {
-        bucket = "prod-sahana-tf-bucket"
-        key = "staging/terraform.tfstate"
-        region = "us-east-1"
-        dynamodb_table = "terraform-lock"
-        encrypt = true
-    }
-}
